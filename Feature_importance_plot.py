329529593ce6ff2d7b8e7d9de8d0b194aa6722a0
Randa Natras <64141673+Randa-lab@users.noreply.github.com>
2023-04-24 13:20:13 +0200

    Update Feature_importance_plot.py

--- Feature_importance_plot.py
+++ Feature_importance_plot.py
@@ -13,3 +13,10 @@ def feature_importance_plot (model, features, VTEC_point):
   plt.rcParams ['figure.figsize'] = [3.6, 5]
   plt.rcParams.update({'font.size': 12})
   plt.show()
+  
+
+## features_70 = X_70_test_df.columns[:]
+## features_40 = X_40_test_df.columns[:]
+## features_10 = X_10_test_df.columns[:]
+
+## feature_importance_plot (qgb_UB_10E70N, features_70, '10E 70N, UB')
